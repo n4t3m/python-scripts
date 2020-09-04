6118e391cb9cc0e2806c608d12ade17ee4e81dce
NateM135 <melwaninate@gmail.com>
2020-09-03 20:51:40 -0700

    Added BullyBot - See README for details

---
+++ bullybot/README.md
@@ -0,0 +1,11 @@
+BullyBot is a bot based on the concept of making it hard for someone to use Discord in the server it is active in.
+
+It accomplishes this using two functions: insta-deleting messages and permanent server mute.
+
+When someone attempts to send a message, it is instantly deleted by the bot. 
+
+When someone attempts to remove the server mute on them, it server mutes them again.
+
+See the video below to see it in action.
+
+<video will be added soon, bot is being rewritten with good practices in mind>
---
+++ bullybot/bully.py
@@ -0,0 +1,85 @@
+import discord
+from discord.ext import commands
+import requests
+import setup
+import random
+import requests
+from discord import Webhook, RequestsWebhookAdapter
+import json
+
+
+description = "Type $info to see more information."
+TOKEN = setup.TOK
+client = commands.Bot(command_prefix='!', description=description, help_command=None)
+client.UID = 0
+client.GUILDID = 0
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    initcount = 0
+    for g in client.guilds:
+        initcount = initcount + len(g.members)
+    game = discord.Game("azusa")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+@client.event
+async def on_guild_join(guild):
+    print("Joined New Server: " + guild.name + ". Members: " + str( len(guild.members) ) + ". Owner: " + guild.owner.display_name + ".")
+
+@client.event
+async def on_guild_remove(guild):
+    print("Removed From Server: " + guild.name + ". Members: " + str( len(guild.members) ) + ". Owner: " + guild.owner.display_name + ".")
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    print('yahllo')
+    print(member.id)
+    print(client.UID)
+    if member.id != client.UID:
+        print('wehere')
+        print(member.id == client.UID)
+        return
+    print('made it here')
+    await member.edit(mute=True)
+    return
+    
+
+    
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:   
+        return
+
+    if int(message.author.id) == int(client.UID):
+        print("liberal destroyed")
+        await message.delete()  
+        
+    await client.process_commands(message)
+
+
+@client.command()
+async def bullyuser(ctx, id):
+    client.UID = int(id)
+    client.GUILDID = ctx.guild.id
+    await ctx.send("Now Bullying <@!" + str(client.UID) + ">")
+
+@client.command()
+async def bullycheck(ctx):
+    await ctx.send("Now Bullying: " + str(client.UID))
+
+@client.command()
+async def reset(ctx):
+    client.UID = 0
+    client.GUILDID = 0
+    await ctx.send("Done")
+    
+    
+  
+
+
+client.run(TOKEN)
+            
