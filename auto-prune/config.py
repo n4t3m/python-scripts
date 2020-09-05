c2fa491ece3e4acc32ad2d45ca9fed432da90cb3
NateM135 <melwaninate@gmail.com>
2020-09-05 10:28:33 -0700

    Added Configuration and JSON Psuedo-Database Files

---
+++ auto-prune/config.py
@@ -0,0 +1,5 @@
+TOK = ""
+PREFIX = "pr!"
+STATUS = "Current Demo: Auto-Prune"
+DEFAULT_DELAY = 300
+DESC = "Auto-Prune Bot. Automatically delete messages in a channel after a certain amount of time."
